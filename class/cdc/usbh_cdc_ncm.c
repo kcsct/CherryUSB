fbf50df0d71ebc4753737e984522a18b9c33a1ce
Pierre de Vos <pdevos@kocos.co.za>
2025-11-12 10:44:55 +0200

    fix(host): respect gadget NTB datagram limit

--- class/cdc/usbh_cdc_ncm.c
+++ class/cdc/usbh_cdc_ncm.c
@@ -194,9 +194,6 @@ static int usbh_cdc_ncm_configure(struct usbh_cdc_ncm *cdc_ncm_class)
     }
 
     host_ntb_in_datagrams = cdc_ncm_class->ntb_param.wNtbOutMaxDatagrams;
-    if (host_ntb_in_datagrams == 0) {
-        host_ntb_in_datagrams = 1;
-    }
 
     ret = usbh_cdc_ncm_set_ntb_input_size(cdc_ncm_class, host_ntb_in_size, host_ntb_in_datagrams);
     if (ret < 0) {
